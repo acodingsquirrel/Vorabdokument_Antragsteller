ce30eccd6951630d7a7936a6c3b4e2067445ffef
acodingsquirrel <lindnerc92@gmail.com>
2020-04-14 10:38:22 +0200

    naechster Zwischenstand

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from random import randrange
 import api_client
 
@@ -9,16 +9,20 @@ app.config['SECRET_KEY']='\x8d\x14\xd2 a\xb3Zl\xf2\xe2\xc1\xac\r\xc6\xe2\x15\xc3
 def login():
    return render_template('login.html')
 
-@app.route('/bestaetigt', methods=['get', 'post'] )
-def bestaetigt():
+@app.route('/pruefelogin', methods=['get', 'post'] )
+def pruefelogin():
     user=request.form['partnerid']
     passwort=request.form['passwort']
     token, loginSuccessful = api_client.getToken(user, passwort)
     if loginSuccessful:
         session['token']= token
-        return render_template('bestaetigt.html',token=token)
+        return redirect('/bestaetigt')
     else:
         return render_template('login.html')
+
+@app.route('/bestaetigt')
+def bestaetigt():
+   return render_template('bex.html', token=session.get('token'))
 
 if __name__ == '__main__':
     app.run(debug=True)
