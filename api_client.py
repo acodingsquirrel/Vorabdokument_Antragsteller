b4d7e7a990fb3c28ff17c305b9a882e14238b99d
acodingsquirrel <lindnerc92@gmail.com>
2020-04-16 14:56:46 +0200

    fuer bex angepasst

--- api_client.py
+++ api_client.py
@@ -2,6 +2,18 @@ import requests
 #ort an dem wir alle api funktionen hinschreiben
 #wenn man innerhalb einer methode eine variable vergibt, ist diese ausserhalb der methode nicht verwendbar
 
+def getPruefen(user, password):
+   credentials = {'username':user, 'password':password}
+   r = requests.post('https://api.europace.de/login', data=credentials)
+   print (r)
+   if r.status_code/100==2:
+       token=r.json()['access_token']
+       loginSuccessful = True
+   else:
+       token=""
+       loginSuccessful = False
+   return token, loginSuccessful
+
 def getToken(user, password):
    credentials = {'username':user, 'password':password}
    r = requests.post('https://api.europace.de/login', data=credentials)
